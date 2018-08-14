c87446c016bd037b70efffb0f8c525ddb2570c4c
Ryoichi Gokan <nargok@gmail.com>
2018-08-14 10:49:01 +0900

    user variable in template

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ server.listen(3000);
 console.log('Server start!');
 
 function getFromClient(req, res) {
-  var content = ejs.render(index_page);
+  var content = ejs.render(index_page, {
+    title: 'Indexページ',
+    content: 'これはテンプレートを使ったサンプルページです',
+  });
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write(content)
   res.end()
